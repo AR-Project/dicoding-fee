4b85e9df332050d676dc8b97a1bc9f5dc45460ae
AR Project <alvenresta@gmail.com>
2023-08-07 16:44:22 +0700

    clean up

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,21 +2,15 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import data from '../public/data/DATA.json'
 
-console.log('Hello Coders! :)');
-
 const hamburgerButtonElement = document.querySelector('#hamburger');
 const drawerCloseBtnElement = document.querySelector('#close-btn')
 const drawerElement = document.querySelector('#drawer');
 const mainElement = document.querySelector('main');
 
-
-
-
 hamburgerButtonElement.addEventListener('click', event => {
   drawerElement.classList.toggle('open');
   event.stopPropagation();
 });
-
 
 drawerCloseBtnElement.addEventListener('click', event => {
   drawerElement.classList.remove('open');
@@ -27,7 +21,3 @@ mainElement.addEventListener('click', event => {
   drawerElement.classList.remove('open');
   event.stopPropagation();
 })
-
-data.restaurants.forEach(restaurant => {
-  console.log(restaurant.name);
-});
